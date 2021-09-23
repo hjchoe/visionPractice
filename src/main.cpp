adb2dd13482e25b14790f613bee6027588fd3822
James Choe <60300779+hjchoe@users.noreply.github.com>
2021-09-22 21:04:32 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,20 +17,25 @@
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 // |---------- Library Imports ----------|
+
 #include "vex.h"
 using namespace vex;
 
 // |---------- Initializing Global Variables ----------|
+
 int centerFieldX = 158;         // half the number of pixels of the vision sensor
 int bottomFieldY = 190;         // bottom of vision sensor field of view
 double speedMultiplier = 0.35;  // multiplier used when calculating speed based on distance from object
 
 // |---------- Functions ----------|
-bool find(signature sig)
+
+// find(object): function that returns a boolean of whether the object is in vision
+bool find(signature sig) // sig parameter is the object to detect
 {
+  // Takes snapshot with vision camera and returns number of signature objects in frame
   int objects = Vision5.takeSnapshot(sig);
-  if (objects != 0) return true;
-  else return false;
+  if (objects == 0) return false;  // return false if no objects found
+  else return true;                // return true if objects in vision
 }
 
 // focus(object): function that rotates the robot until the object in vision is centered on the x-axis
@@ -45,11 +50,12 @@ void focus(signature sig) // sig parameter is the object to detect
   // while loop until robot is lined up with object in vision on the x-axis
   while (!linedUp)
   {
-    // Takes snapshot with vision camera and returns number of signature objects in frame
+    // take snapshot with vision camera and return number of signature objects in frame
     objects = Vision5.takeSnapshot(sig);
+    // store the largest object's x-coordinate
     x = Vision5.largestObject.centerX;
 
-    // If no object in vision
+    // if no object in vision
     if (objects != 0)
     {
       // print "no object" to controller
@@ -64,10 +70,10 @@ void focus(signature sig) // sig parameter is the object to detect
       return;
     }
 
-    // If objects in vision
+    // if objects in vision
     else
     {
-      // If the object's x-coordinate is to the left of the center of vision
+      // if the object's x-coordinate is to the left of the center of vision
       if (x > centerFieldX + 10)       // 10 is added to the center x value to give a 10 pixel band to the target
       {
         // print "right" direction to controller
@@ -82,7 +88,7 @@ void focus(signature sig) // sig parameter is the object to detect
         rightMotor.setVelocity(0, velocityUnits::pct);
       }
 
-      // If the object's x-coordinate is to the right of the center of vision
+      // if the object's x-coordinate is to the right of the center of vision
       else if (x < centerFieldX - 10)  // 10 is subtracted to the center x value to give a 10 pixel band to the target
       {
         // print "left" direction to controller
@@ -97,7 +103,7 @@ void focus(signature sig) // sig parameter is the object to detect
         rightMotor.setVelocity(speed, velocityUnits::pct);
       }
 
-      // object is in vision and centered on the x-axis
+      // if object is in vision and centered on the x-axis
       else
       {
         // set motor velocities to 0 (stop motors)
@@ -118,12 +124,22 @@ void focus(signature sig) // sig parameter is the object to detect
   }
 }
 
-void approach(signature sig)
+// approach(object): function that approaches object in frame until the object is at the bottom of vision
+void approach(signature sig) // sig parameter is the object to detect
 {
-  LinedUp = false;
-  while (!LinedUp)
+  // Initializing variables
+  int objects = 0;
+  int y = 0;
+  double speed = 0;
+  bool linedUp = false;
+
+  // while loop until robot is lined up with bottom of vision on the y-axis
+  while (!linedUp)
   {
-    int objects = Vision5.takeSnapshot(sig);
+    // take snapshot with vision camera and return number of signature objects in frame
+    objects = Vision5.takeSnapshot(sig);
+    // store the largest object's y-coordinate
+    y = Vision5.largestObject.centerY;
 
     if (objects != 0)
     {
